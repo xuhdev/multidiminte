3fcbea55871c5f1e1594940e01403dc87d8e43bc
Hong Xu <hong@topbug.net>
2013-12-10 17:51:13 -0800

    Add a test for 3-dim integral.

--- src/test.c
+++ src/test.c
@@ -8,11 +8,6 @@ static double test_1_f(const double* x, size_t n, void* p)
 
 #define test_2_f test_1_f
 
-static double test_2_x1(const double* x, size_t n, void * p)
-{
-    return *x;
-}
-
 /*
  * \int_0^2 dy \int_0^1 dx x^2
  *
@@ -90,6 +85,11 @@ static int test_1(void)
     return ret;
 }
 
+static double test_2_x1(const double* x, size_t n, void * p)
+{
+    return *x;
+}
+
 /*
  * \int_0^2 dy \int_0^y dx x^2
  */
@@ -152,7 +152,82 @@ static int test_2(void)
     return ret;
 }
 
+
+#define test_3_f test_1_f
+
+static double test_3_x1(const double* x, size_t n, void * p)
+{
+    return *(x + 1);
+}
+
+
+/*
+ * \int_1^3 dz \int_0^2 dy \int_0^y dx x^2
+ */
+static int test_3(void)
+{
+    gmdi_inte_handle            handle = gmdi_create_inte_handle(3);
+    gmdi_function_or_constant   fc;
+    gmdi_multi_var_function     mvf;
+    int                         ret = 0;
+
+    gmdi_handle_set_key(handle, GSL_INTEG_GAUSS61, 0);
+    gmdi_handle_set_key(handle, GSL_INTEG_GAUSS61, 1);
+    gmdi_handle_set_key(handle, GSL_INTEG_GAUSS61, 2);
+    gmdi_handle_set_epsabs(handle, 0, 0);
+    gmdi_handle_set_epsabs(handle, 0, 1);
+    gmdi_handle_set_epsabs(handle, 0, 2);
+    gmdi_handle_set_epsrel(handle, 1e-7, 0);
+    gmdi_handle_set_epsrel(handle, 1e-7, 1);
+    gmdi_handle_set_epsrel(handle, 1e-7, 2);
+    gmdi_handle_set_limit(handle, 100000, 0);
+    gmdi_handle_set_limit(handle, 100000, 1);
+    gmdi_handle_set_limit(handle, 100000, 2);
+    gmdi_handle_set_inte_func(handle, GMDI_INTE_FUNCTIONS_QAG, 0);
+    gmdi_handle_set_inte_func(handle, GMDI_INTE_FUNCTIONS_QAG, 1);
+    gmdi_handle_set_inte_func(handle, GMDI_INTE_FUNCTIONS_QAG, 2);
+
+    gmdi_handle_set_x0_constant(handle, 1, 0);
+    gmdi_handle_set_x0_constant(handle, 0, 1);
+    gmdi_handle_set_x0_constant(handle, 0, 2);
+
+    gmdi_handle_set_x1_constant(handle, 3, 0);
+    gmdi_handle_set_x1_constant(handle, 2, 1);
+
+    fc.type = GMDI_FUNCTION_OR_CONSTANT_TYPE_MULTI_VAR_FUNCTION;
+    fc.content.mf.function = test_3_x1;
+    fc.content.mf.params = NULL;
+    gmdi_handle_set_x1(handle, &fc, 2);
+
+    mvf.n = 1;
+    mvf.function = test_3_f;
+    mvf.params = NULL;
+    gmdi_handle_set_kernel(handle, &mvf);
+
+    gmdi_multi_dimensional_integration(handle);
+
+    printf("Test 3: \\int_1^3 dz \\int_0^2 dy \\int_0^y dx x^2\n");
+    puts("The result should be 8/3");
+    puts("Method: gsl_integration_qag");
+    printf("result: %e\n", gmdi_handle_get_result(handle));
+    printf("abserr: %e\n", gmdi_handle_get_abserr(handle));
+
+    if (fabs(gmdi_handle_get_result(handle) - 8.0/3) >= 1e-7)
+    {
+        puts("Test failed");
+        ret = 1;
+    }
+    else
+        puts("Test passed");
+
+    puts("");
+
+    gmdi_free_inte_handle(handle);
+
+    return ret;
+}
+
 int main(int argc, const char *argv[])
 {
-    return test_1() || test_2();
+    return test_1() || test_2() || test_3();
 }
